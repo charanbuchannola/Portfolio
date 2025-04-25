d960e995f50d2f2bfd796a736202491ba90c240b
charanbuchannola <buchannolacharan@gmail.com>
2025-04-25 13:48:03 +0530

    Add TypeScript ignore comment for Cards component mapping

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -43,6 +43,7 @@ const Projects = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-10">
           {projectJson.map((items) => {
+            // @ts-ignore
             return <Cards item={items} />;
           })}
         </div>
